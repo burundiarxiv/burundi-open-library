bca809be3c72662476fbfbbb585cb73d03a29a91
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2020-11-15 01:01:35 +0100

    Convert into functional component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/CardList.js";
 import "./App.css";
 //import SearchBox from "./components/search-box/SearchBox.js";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [library, setLibrary] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    this.state = {
-      books: [],
-    };
-  }
-  componentDidMount() {
+  useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/burundiarxiv/library-parser/master/export/chap-2-1-2.json"
     )
       .then((response) => response.json())
-      .then((users) => this.setState({ books: users }));
-  }
-  /* handleChange = (e) => {
-    this.setState({ searchFiled: e.target.value });
+      .then((data) => {
+        setLibrary(data);
+        setBooks(data);
+      });
+  }, []);
+
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    if (value === "") {
+      setBooks(library);
+      return;
+    }
+    const filteredBooks = library.filter(({ line }) =>
+      line.toLowerCase().includes(value.toLowerCase())
+    );
+    setBooks(filteredBooks);
   };
- */
-  render() {
-    /* const { books, searchFiled } = this.state;
-    const filterbooks = books.filter((book) =>
-      book.line.toLowerCase().includes(searchFiled.toLowerCase())
-    ); */
-    return (
+
+  return (
+    <>
       <div className="App">
         <header>
           <h1>Burundi Open Library</h1>
-          <p className="title">Free and open</p>
+          <p className="title">
+            Free and open access to a general library on books written on
+            Burundi
+          </p>
           <div className="search">
             <input
               type="search"
               placeholder="Search for books"
-              onChange={this.handleChange}
+              onChange={onChangeHandler}
             />
           </div>
         </header>
         <body>
-          {/* <SearchBox
-            placeholder="search books"
-            handleChange={this.handleChange}
-          /> */}
-          <CardList books={this.state.books} />
+          <CardList books={books} />
         </body>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default App;
