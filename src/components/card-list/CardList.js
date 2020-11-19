20b7ab4457797be77cec1915cb1643ee29d8fe20
Muco Rolle Tresor <mucotresor@gmail.com>
2020-11-19 18:58:46 +0200

    feat: Add chakra ui and redesign the ui.

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,14 +1,19 @@
-import React from "react";
-import Card from "../card.js/Card";
-import "./CardList.css";
+import { Heading, Box, SimpleGrid } from '@chakra-ui/react';
+import React from 'react';
+import Card from '../card.js/Card';
+import './CardList.css';
 
 const CardList = ({ books }) => (
-  <div className="card-list">
-    <p>{books.length} books found</p>
-    {books.map((book, bookIndex) => (
-      <Card key={bookIndex} book={book} />
-    ))}
-  </div>
+  <Box>
+    <Heading as="h2" m="30px 0">Books found</Heading>
+    
+    <SimpleGrid columns={{sm: 1, md: 2, lg: 3}}  spacing={10}>
+      {books.map((book, bookIndex) => (
+        <Card key={bookIndex} book={book} />
+      ))}
+    </SimpleGrid>
+  </Box>
+    
 );
 
 export default CardList;
