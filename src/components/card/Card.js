4e01eb2807547638aa9ab9ea3b3be5ed98e4a5ca
opethrocks <lebleu131@icloud.com>
2020-11-23 08:22:10 -0600

    #18 add skeleton loader for the books

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Heading, Box, Text } from '@chakra-ui/react';
-const Card = ({ book }) => {
+import { Skeleton } from '@chakra-ui/react';
+const Card = ({ book, books }) => {
   const { title, authors, line } = book;
+
   return (
-    <Box boxShadow="base" p={5} borderRadius="lg" _hover={{boxShadow: "md"}}>
-      <Heading as="h4" size="sm">{title}</Heading>
-      <Text mt={3} mb={3}>
-        <Text as="strong">Auteurs: </Text> 
-        {authors}
-      </Text>
-      
-      <Text>
-        <Text as="strong">Note: </Text> 
+    <Skeleton isLoaded>
+      <Box
+        boxShadow="base"
+        p={5}
+        borderRadius="lg"
+        _hover={{ boxShadow: 'md' }}
+      >
+        <Heading as="h4" size="sm">
+          {title}
+        </Heading>
+        <Text mt={3} mb={3}>
+          <Text as="strong">Auteurs: </Text>
+          {authors}
+        </Text>
+
+        <Text>
+          <Text as="strong">Note: </Text>
           {line}
         </Text>
-    </Box>
+      </Box>
+    </Skeleton>
   );
 };
 
