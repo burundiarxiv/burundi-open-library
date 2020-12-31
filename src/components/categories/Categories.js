1247e9a94d93d8e1676aeb5ee2a674a6d9404073
Mugisha charbel Michael <michaelmugisha23@gmail.com>
2020-12-31 11:02:48 +0200

    add comment on categories

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -3,6 +3,9 @@ import './categories.css';
 const Categories = ({ filterItems, library }) => {
   // The purpose of this three lines is to convert the tags
   // that are in subarray into one array, so that i can map through it
+  // the problem was that when i was trying to map through an
+  // array with subarray, it was returning NaN at the index
+  // of every subarray
   const listOfTags = library.map((item) => item.tags);
   const listOfTagsIntoOneString = listOfTags.toString();
   const convertedListOfTags = listOfTagsIntoOneString.split(',');
