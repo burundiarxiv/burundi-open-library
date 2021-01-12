8a728e1bd0fdbfb2dbb708b830e1db980e339907
Mugisha charbel Michael <michaelmugisha23@gmail.com>
2021-01-12 03:06:02 +0200

    add toggle

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 function Nav() {
+  const [toggle, setToggle] = useState(false);
+  const [toggleType, setToggleType] = useState('fas fa-bars');
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const checkWidth = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', checkWidth);
+    if (width < 600) {
+      setToggle(true);
+    }
+    return () => {
+      window.removeEventListener('resize', checkWidth);
+    };
+  }, [width]);
+  const changeToggle = () => {
+    if (!toggle) {
+      document.getElementById('ul').style.display = 'inline-block';
+      setToggle(true);
+      setToggleType('fas fa-times');
+    } else {
+      document.getElementById('ul').style.display = 'none';
+      setToggle(false);
+      setToggleType('fas fa-bars');
+    }
+  };
   return (
-    <nav>
-      {/*  <input type="checkbox" id="check" />
-      <label for="check" className="checkbtn">
-        <i className="fas fa-bars"></i>
-      </label> */}
-      <ul>
-        <li>
-          <Link to="/" className="#true">
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link to="/about-us" className="#true">
-            Qui sommes-nous
-          </Link>
-        </li>
-        <li>
-          <Link to="/our-work" className="#true">
-            Notre Travail
-          </Link>
-        </li>
-        <li>
-          <Link to="/how-to-use-the-platform">
-            Comment Utiliser La Platforme
-          </Link>
-        </li>
-        <li>
-          <Link to="/bibliographic-content">Contenu Bibliographique</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <button className="icon" onClick={changeToggle}>
+        <i className={toggleType}></i>
+      </button>
+      <nav id="ul">
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/about-us">Qui sommes-nous</Link>
+          </li>
+          <li>
+            <Link to="/our-work">Notre Travail</Link>
+          </li>
+          <li>
+            <Link to="/how-to-use-the-platform">
+              Comment Utiliser La Platforme
+            </Link>
+          </li>
+          <li>
+            <Link to="/bibliographic-content">Contenu Bibliographique</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
